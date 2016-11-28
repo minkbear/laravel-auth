4d49d4081a90e0cee8697954e97f9a3b40227b3e
minkbear <minkbear@gmail.com>
2016-11-29 00:24:47 +0700

    upgrade to laravel 5.3

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Notifications\Notifiable;
 use App\Logic\User\CaptureIp;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-    use Authenticatable, CanResetPassword;
+    use use Notifiable, Authenticatable, CanResetPassword;
 
     /**
      * The database table used by the model.
